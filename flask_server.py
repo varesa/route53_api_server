7c760b77d7cf2002f0060ee2040218613d80eeb4
Esa Varemo <esa@kuivanto.fi>
2016-12-16 23:20:01 +0000

    Finishing up

--- flask_server.py
+++ flask_server.py
@@ -1,22 +1,62 @@
+import traceback
+
 from flask import Flask, request
+
 from r53 import Route53Connection
+
+
 app = Flask(__name__)
+
+debug = False
+
 
 @app.route('/')
 def home():
     return """
     <html><body>
     Please make a POST request to POST with the parameters aws_access_id, aws_secret_access_key, aws_route53_zone, record_name, record_type and record_value.
+    If request_type is 'A' you may omit record_value and the IP address you are connecting from will be used instead.
     </body></html>
     """
 
+
+def make_message(msg):
+    return msg
+
+
 @app.route('/change', methods=['POST'])
 def change():
-    print(request.form)
-    c = Route53Connection(request.form['aws_access_key_id'], request.form['aws_secret_access_key'], request.form['aws_route53_zone'])
-    c.change_record(request.form['record_name'], request.form['record_type'], request.form['record_value'])
-    return """
-    <html><body>
-    OK
-    </body></html>
-    """
+    if debug:    
+        print(request.headers)
+        print(request.form)
+
+    if 'X-Forwarded-For' in request.headers.keys():
+        source_ip = request.headers['X-Forwarded-For']
+    else:
+        source_ip = request.remote_addr
+
+    if debug: print("Client address: " + str(source_ip))  
+  
+
+    # Check for missing parameters
+    if 'aws_access_key_id' not in request.form or 'aws_secret_access_key' not in request.form:
+        return make_message("aws_access_key_id or aws_secret_access_key POST parameters missing")
+
+    if 'aws_route53_zone' not in request.form.keys() or 'record_name' not in request.form.keys():
+        return make_message("aws_route53_zone or record_name POST parameters missing")
+
+
+    # Default to setting A record to client IP if parameters missing
+    record_type = request.form['record_type'] if 'record_type' in request.form.keys() else 'A'
+    record_value = request.form['record_value'] if 'record_value' in request.form.keys() else source_ip
+
+    # Try to update the record
+    ret = ""
+    try:
+        c = Route53Connection(request.form['aws_access_key_id'], request.form['aws_secret_access_key'], request.form['aws_route53_zone'])
+        ret = c.change_record(request.form['record_name'], record_type, record_value)
+    except:
+        print(traceback.format_exc())
+        return make_message("An error occurred")
+
+    return make_message(ret)
--- r53.py
+++ r53.py
@@ -1,22 +1,19 @@
 import route53
 
-AWS_ACCESS_KEY_ID     = "AKIAIQKRT7R5AAO6K6YQ"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_ROUTE53_ZONE      = "Z1HMB761DV99GU"
-
-record       = "tekumo.fi."
-record_type  = "A"
-record_value = "123.123.123.123"
+debug = False
 
 class Route53Connection:
     def __init__(self, aws_access_key_id, aws_secret_access_key, aws_route53_zone):
-        conn = route53.connect(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-        self.zone = conn.get_hosted_zone_by_id(AWS_ROUTE53_ZONE)
+        conn = route53.connect(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        self.zone = conn.get_hosted_zone_by_id(aws_route53_zone)
 
     def change_record(self, record_name, record_type, record_value):
         for record_set in self.zone.record_sets:
-            if record_set.rrset_type == record_type and record_set.name == record:
-                #print("match: {} {} {}".format(record_set.name, record_set.rrset_type, record_set.records[0]))
+            if debug: print("record: {} {} {}".format(record_set.name, record_set.rrset_type, record_set.records[0]))
+            if record_set.rrset_type == record_type and record_set.name == record_name:
+                if debug: print("match: {} {} {}".format(record_set.name, record_set.rrset_type, record_set.records[0]))
                 record_set.records = [record_value]
                 record_set.save()
+                return "OK"
+        return "Record not found"
 
